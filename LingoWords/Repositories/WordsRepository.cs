c4cb259fd07b9776578c4a1c1ce4956d68e33288
Luuk de Jong <32489062+LuukdeJong123@users.noreply.github.com>
2020-05-09 15:35:09 +0200

    First push

---
+++ LingoWords/Repositories/WordsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LingoWords.Repositories
+{
+    public class WordsRepository
+    {
+    }
+}
